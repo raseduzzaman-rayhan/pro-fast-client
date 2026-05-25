83ca9a5cb0e633b78c714d48d3c63af89d9afa46
raseduzzaman-rayhan <raseduzzamanrayhan@gmail.com>
2026-05-25 13:02:04 +0600

    Setup initial React project structure with layouts and shared components

---
+++ src/router/router.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home/Home/Home";
+import RootLayout from "../layouts/RootLayout";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: RootLayout,
+        children: [
+            {
+                index: true,
+                Component: Home
+            },
+            {
+                
+            }
+        ]
+    },
+]);
